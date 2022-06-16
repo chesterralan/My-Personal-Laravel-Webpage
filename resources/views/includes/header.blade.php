04041ebc6864e6134b84bc36ef27c403bf5d1757
Chester Calingasan <101789697+chesterralan@users.noreply.github.com>
2022-06-17 00:31:27 +0800

    Add files via upload

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,86 @@
+<style>
+h1   {
+    color: #000;
+    font-size:50px;
+}
+h2   {
+    color:#808080;
+    }
+p{
+    font-size:18px;
+}
+a{
+    color:#fff;
+    font-size:20px;
+    text-decoration:none;
+    font-family: 'Bebas Neue', cursive;
+}
+ul{
+    list-style:none;
+}
+body {
+    background-color: #fff;
+    margin-left: 0px;
+    margin-top: 0px;
+    margin-right: 0px;
+    margin-bottom: 0px;
+    font-family: 'Bebas Neue', cursive;
+}
+.instruction{
+    width:90%;
+    height:500px;
+    padding-left:5%;
+    padding-top:10%;
+}
+.instruction2{
+    width:100%;
+    height:400px;
+    padding-top:15%;
+    text-align:right;
+}
+.instruction3{
+    width:90%;
+    height:300px;
+}
+.instruction4{
+    width:100%;
+    height:400px;
+    padding-top:15%;
+    text-align:right;
+}
+.navbar{
+    width:100%;
+    height:70px;
+    background-color:#000;
+    position:fixed;
+    color:#fff;
+    font-size:10px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+}
+.navbar-inner{
+    width:50%;
+    margin-left:5%;
+}
+.navbar-inner-right{
+    justify-content:center;
+    display: flex;
+    margin-left:35%;
+    grid-gap:10%;
+    text-align: left;
+    width:50%;
+    text-align:right;
+}
+</style>
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="/">Chester Calingasan</a>
+        </div>
+        <div class="navbar-inner-right">
+            <a href="/">Home</a>
+            <a href="/about">Contact</a>
+</div>
+
+
+</div>
